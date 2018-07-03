89b91c980ce606dd026a433870ca0fe85e1bc85d
Michael Carlos <michaelpcarlos@gmail.com>
2018-07-03 16:46:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
-const katzDeli = [];
-
 function takeANumber(line, name) {
   line.push(name);
-
+  
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
-
-
 function nowServing(line) {
-  if (!line.length) {
-    return "There is nobody waiting to be served!";
-  }
-  
-  return `Currently serving ${line.shift()}.`;
+  return line.length === 0 ? 'There is nobody waiting to be served!' : `Currently serving ${line.shift()}.`;
 }
 
-
 function currentLine(line) {
-  if (!line.length) {
-    return "The line is currently empty.";
+  if (line.length === 0) {
+    return 'The line is currently empty.';
   }
-
-  const phrase = [];
-
-  for (let i = 0, l = line.length; i < l; i++) {
-    phrase.push(`${i + 1}. ${line[i]}`);
+  
+  var arr = [];
+  
+  for (var i = 0; i < line.length; i++) {
+    var names = line[i];
+    
+    arr.push(`${i + 1}. ${names}`);
   }
-
-  return `The line is currently: ${phrase.join(', ')}`;
+  
+  return `The line is currently: ${arr.join(', ')}`;
 }
